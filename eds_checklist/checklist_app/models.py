7c04c6db0f4592e9dd6835c618e1b3306aa512ae
Mario Roberto <mrgomez.dev@gmail.com>
2024-10-15 10:09:16 -0600

    Creacion de modelos para checklis, tareas, eds

--- eds_checklist/checklist_app/models.py
+++ eds_checklist/checklist_app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class EDS(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=255)
+    status = models.CharField(max_length=50, default='Pendiente')
+
+    def __str__(self):
+        return self.name
+
+
+class Checklist(models.Model):
+    eds = models.ForeignKey(EDS, on_delete=models.CASCADE, related_name='checklists')
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    start_date = models.DateField()
+    end_date = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.title} - {self.eds.name}'
+
+
+class Task(models.Model):
+    checklist = models.ForeignKey(Checklist, on_delete=models.CASCADE, related_name='tasks')
+    description = models.CharField(max_length=255)
+    start_time = models.TimeField()
+    duration = models.DurationField()
+    is_completed = models.BooleanField(default=False)
+    comments = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.description
