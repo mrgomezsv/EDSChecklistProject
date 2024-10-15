3c1b9b06190df35e455caf3be98b8f74f4c1dd6d
Mario Roberto <mrgomez.dev@gmail.com>
2024-10-15 10:10:07 -0600

    Se extiende modelo para manejo de roles

--- eds_checklist/checklist_app/models.py
+++ eds_checklist/checklist_app/models.py
@@ -1,4 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('ADMIN', 'Administrador'),
+        ('TRAINER', 'Capacitador'),
+        ('OPERATOR', 'Operador'),
+    )
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='OPERATOR')
+
+    def __str__(self):
+        return self.username
+
+
 
 class EDS(models.Model):
     name = models.CharField(max_length=100)
