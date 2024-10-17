2a2c889c4ff1064e1642c6cf776b70cec3135b48
Mario Roberto <mrgomez.dev@gmail.com>
2024-10-17 08:42:21 -0600

    Se establecieron mensajes de warning

--- checklist_app/views.py
+++ checklist_app/views.py
@@ -6,7 +6,10 @@ from django.contrib.auth.decorators import login_required
 from rest_framework import viewsets
 from .models import EDS, Checklist, Task
 from .serializers import EDSSerializer, ChecklistSerializer, TaskSerializer
-from .forms import CustomUserCreationForm
+from django.shortcuts import render, redirect
+from checklist_app.forms import CustomUserCreationForm  # Importar el formulario personalizado
+from django.contrib.auth import login
+from django.contrib import messages
 
 # Vista para el dashboard (requiere login)
 @login_required
@@ -34,15 +37,17 @@ def task_list(request):
     return render(request, 'task_list.html')
 
 
-
 def register(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
+        if form.is_valid():  # Verificar si el formulario es válido
+            user = form.save()  # Guardar el nuevo usuario
+            login(request, user)  # Iniciar sesión automáticamente
             messages.success(request, 'Usuario registrado con éxito.')
-            return redirect('dashboard')
+            return redirect('dashboard')  # Redirigir al dashboard
+        else:
+            print(form.errors)  # Imprimir errores del formulario en la consola para depuración
     else:
         form = CustomUserCreationForm()
+
     return render(request, 'registration/register.html', {'form': form})
--- settings.py
+++ settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,6 +76,15 @@ WSGI_APPLICATION = 'wsgi.application'
 LOGIN_URL = '/login/'  # Corregido
 LOGIN_REDIRECT_URL = 'dashboard'  # Redirige al dashboard después del login
 LOGOUT_REDIRECT_URL = 'login'  # Redirige al login después del logout
+
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
 
 # Database
